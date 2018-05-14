eb6d0f089255f973e1845f425e4ead8c89d7e33d
Dunphyal <dunphyal@tcd.ie>
2018-05-14 16:09:52 +0100

    Add files via upload

---
+++ SceneManagement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagemenet : MonoBehaviour
+{
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("AR_Module_Test");
+
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
+
